44f9f17cdf7b47da33da5f01d61709ded86e4801
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-06-19 13:53:02 -0700

    adds horoscope api links

--- js/index.js
+++ js/index.js
@@ -4,4 +4,6 @@ using an api for horoscope data
 using an ajax/xhtml call to retrieve information
 
 found free api for horoscope
+https://www.quora.com/What-are-some-free-horoscope-APIs
+http://sandipbgt.com/theastrologer/api/
 */
