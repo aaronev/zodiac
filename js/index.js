fc7b9d0dcd56b54b0ccd1e17ee50a26386ed4683
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-06-20 18:58:01 -0700

    sets up a tes for var xhr = new XMLHttpRequest()

--- js/index.js
+++ js/index.js
@@ -8,4 +8,20 @@ https://www.quora.com/What-are-some-free-horoscope-APIs
 http://sandipbgt.com/theastrologer/api/
 http://sandipbgt.com/theastrologer/api/horoscope/taurus/today/
 http://sandipbgt.com/theastrologer/api/horoscope/taurus/yesterday/
+
+
+1 find way to make request to api and get information to render
+the simplest way to do this is by hard coding a sign first into api
+http://sandipbgt.com/theastrologer/api/horoscope/taurus/today/
+
+2. then render information to html
+
+var xhr = new XMLHttpRequest()
+
 */
+
+
+//test out var xhr = new XMLHttpRequest()
+
+var xhr = new XMLHttpRequest()
+
