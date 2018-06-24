35abad32bea3d2296e2a9f07ff1288802183f14a
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-06-24 14:35:11 -0700

    completes the MVP needs more styling and it should be good to go

--- js/index.js
+++ js/index.js
@@ -1,14 +1,43 @@
+document.getElementById("home").addEventListener("click", function() {
+  document.getElementById("select").style.display="block"
+  document.getElementById("result").style.display="none"
+})
+
+//uses event listern to make a ajax call to retrieve info from an api
 document.getElementById("signs").addEventListener("click",
-  function (e) {
+  function(e) {
     const sign = e.target.textContent.toLowerCase()
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("horoscope").innerHTML = JSON.parse(this.responseText).horoscope;
+    document.getElementById("result").style.display="block"
+    const allSigns = [
+      "taurus", 
+      "aquarius", 
+      "pisces", 
+      "aries", 
+      "gemini", 
+      "cancer", 
+      "leo", 
+      "virgo", 
+      "libra", 
+      "scorpio", 
+      "sagittarius", 
+      "capricorn"
+    ]
+    if (allSigns.includes(sign)) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          document.getElementById("horoscope").innerHTML = JSON.parse(this.responseText).horoscope;
+        }
+      };
+      xhttp.open("GET", `http://sandipbgt.com/theastrologer/api/horoscope/${sign}/today/`, true);
+      xhttp.send();
+      var allImg = document.getElementsByTagName('IMG')
+      for (let i = 0; i < allImg.length; i++) {
+        allImg[i].style.display="none"
       }
-    };
-    xhttp.open("GET", `http://sandipbgt.com/theastrologer/api/horoscope/${sign}/today/`, true);
-    xhttp.send();
-    document.getElementById(sign).style.display="block";
+      document.getElementById("select").style.display="none"
+      document.getElementById(sign).style.display="block";
+    }
   }
 )
+
