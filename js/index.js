6c83d2ebf2cbea4b431541fc602a9cb97aeb41d5
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-06-27 19:34:53 -0700

    cleans up html by having only one img tag and use js to manipulate it and change the img everytime function is called

--- js/index.js
+++ js/index.js
@@ -19,22 +19,22 @@ document.getElementById("signs").addEventListener("click",
     //targets events and helps me get the text within the li element
     const sign = e.target.textContent.toLowerCase()
     //all Signs will help me with the boolean condition
-    const allSigns = [
-      "taurus", 
-      "aquarius", 
-      "pisces", 
-      "aries", 
-      "gemini", 
-      "cancer", 
-      "leo", 
-      "virgo", 
-      "libra", 
-      "scorpio", 
-      "sagittarius", 
-      "capricorn"
-    ]
+    const allSigns = {
+      taurus: "https://www.astrologyzone.com/wp-content/themes/JointsWP-master/assets/images/horoscopes/horoscope-article-hero/az_img_horoscope_taurus.png",
+      aquarius: "https://i2.wp.com/numerologydailyreport.com/wp-content/uploads/2017/07/Aquarius-Traits2.png?fit=1254%2C700&ssl=1", 
+      pisces: "http://identity-mag.com/wp-content/uploads/2018/03/pisces.png", 
+      aries: "https://i2.wp.com/numerologydailyreport.com/wp-content/uploads/2017/07/aries-traits.jpg?fit=770%2C538&ssl=1", 
+      gemini: "http://www.izodiaque.com/wp-content/uploads/2017/05/Gemini.png", 
+      cancer: "https://piedfeed.com/wp-content/uploads/2017/08/horoscope-cancer-sign.jpg", 
+      leo: "http://myentertainmentspot.com/wp-content/uploads/2017/11/az_img_horoscope_leo-660x330.png", 
+      virgo: "https://www.astrologyzone.com/wp-content/themes/JointsWP-master/assets/images/horoscopes/horoscope-article-hero/az_img_horoscope_virgo.png", 
+      libra: "https://www.astrologyzone.com/wp-content/themes/JointsWP-master/assets/images/horoscopes/horoscope-article-hero/az_img_horoscope_libra.png", 
+      scorpio: "https://www.astrologyzone.com/wp-content/themes/JointsWP-master/assets/images/horoscopes/horoscope-article-hero/az_img_horoscope_scorpio.png", 
+      sagittarius: "https://www.astrologyzone.com/wp-content/themes/JointsWP-master/assets/images/horoscopes/horoscope-article-hero/az_img_horoscope_sagittarius.png", 
+      capricorn: "https://www.astrologyzone.com/wp-content/themes/JointsWP-master/assets/images/horoscopes/horoscope-article-hero/az_img_horoscope_capricorn.png"
+    }
     //this condition helps me execute what is needed if the user clicks an li element with text
-    if (allSigns.includes(sign)) {
+    if (Object.keys(allSigns).includes(sign)) {
       //once it finds a text that is one of the horoscope this function shows the result
       cx().result.style.display="block"
       
